e49fb635b04042e12d3c7aa71a9908d37f2ac92c
Jin <bidulgi02@nate.com>
2013-08-21 03:22:39 +0900

    Color

---
+++ SOC/Color.h
@@ -0,0 +1,65 @@
+#pragma once
+
+namespace Rendering
+{
+	struct Color
+	{
+	public:
+		float a;
+		float b;
+		float g;
+		float r;
+
+	public:
+		Color();
+		Color(float r, float g, float b);
+		Color(float r, float g, float b, float a);
+
+	public:
+		Color operator -(Color c);
+		bool operator !=(Color c);
+		Color operator *(Color c);
+		Color operator *(float f);
+		Color operator /(float f);
+		Color operator +(Color c);
+		bool operator ==(Color c);
+
+	public:
+		static Color black();
+		static Color blue();
+		static Color clear();
+		static Color cyan();
+
+		static Color gray();
+
+		float grayscale();
+
+		static Color green();		
+		static Color magenta();		
+		static Color red();		
+		static Color white();		
+		static Color yellow();
+
+		static Color Lerp(Color a, Color b, float t);
+
+	public:
+		Color& GetColor()
+		{
+			return (*this); 
+		}
+
+	public:
+		void SetColor(Color &color)
+		{
+			r = color.r;
+			g = color.g;
+			b = color.b;
+			a = color.a;
+			CalculateColor(this);
+		}
+
+	private:
+		void CalculateColor(Color *color);
+	};
+
+}
