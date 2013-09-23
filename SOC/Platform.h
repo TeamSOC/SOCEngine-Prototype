601d25b68d90a2ac7865f97a939598b07085c06e
Aven <zmfltbsk2@naver.com>
2013-09-24 02:16:10 +0900

    add FBX SDK Link : http://usa.autodesk.com/adsk/servlet/pc/item?siteID=123112&id=10775847

--- SOC/Platform.h
+++ SOC/Platform.h
@@ -2,6 +2,10 @@
 
 #include "RenderingMath.h"
 
+#define FBXSDK_NEW_API
+#pragma comment(lib, "libfbxsdk-md.lib")
+#include <fbxsdk.h>
+
 #if defined(WIN32) && !defined(_USE_GL_DEFINES)
 
 #include "DX.h"
